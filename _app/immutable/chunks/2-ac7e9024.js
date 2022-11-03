802d6982ce240d3d3c695fa24708b8335680f8c1
hansuribe <hansuribe@gmail.com>
2022-11-03 07:35:17 -0500

    Updates

--- _app/immutable/chunks/2-ac7e9024.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-02aa76bd.js";export{t as component};
--- _app/immutable/chunks/3-0a828c27.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-27a9b413.js";export{t as component};
