bc318027c2cc4c173ac8aacaeb8b1a13f8f24c20
hansuribe <hansuribe@gmail.com>
2022-11-04 22:17:46 -0500

    Updates

---
+++ _app/immutable/chunks/1-00bd4105.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4e3bfc74.js";export{t as component};
--- _app/immutable/chunks/1-5d6a795e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-58430a6e.js";export{t as component};
